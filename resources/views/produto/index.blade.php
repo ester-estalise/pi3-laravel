cddf40ef94230cce99533093145b8f797e9a212f
Ester Estalise <ester.estalise@yooper.com.br>
2023-05-09 21:44:19 -0300

    atualizei a home

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 @section('main')
 
+<main class="introducao-bg">
+    <div class="introducao container">
+      <div class="introducao-conteudo">
+        <h1 class="font-1-xxl cor-5">Instrumentos de primeira linha para você<span class="cor-p1">.</span></h1>
+        <p class="font-2-l cor-5">Só aqui na ECHO você encontra os melhores instrumentos para sua banda, ou para curtir um Karaokê com a família.
+        </p>
 
+      </div>
+      <picture>
+        <source media="(max-width: 800px)" srcset="./img/bicicletas/nimbus.jpg">
+        <img src="./img/fotos/inicial.jpg" width="1280" height="1600" alt="Bicicleta elétrica preta.">
+      </picture>
+    </div>
+  </main>
   <article class="bicicletas-lista">
     <h2 class="container font-1-xxl " style="color:#37767c">Mais vendidos<span class="cor-4">.</span></h2>
 
@@ -30,7 +43,17 @@
 
     </ul>
   </article>
-
+  <section class="depoimento" aria-label="Depoimento">
+    <div>
+      <img src="./img/fotos/depoimento2.jpg" width="1560" height="1360" alt="Pessoa pedalando uma bicicleta Bikcraft">
+    </div>
+    <div class="depoimento-conteudo">
+      <blockquote class="font-1-xl cor-4">
+        <p>A música sempre foi minha paixão, e comprar meus instrumentos na Echo foi a melhor coisa que eu fiz, eu ja tenho todos eles a mais de um ano, e eles só me ajudaram muito mais a intensificar meu amor pela música.</p>
+      </blockquote>
+      <span class="font-1-m-b cor-4">Ana Júlia</span>
+    </div>
+  </section>
 
 
 @endsection
